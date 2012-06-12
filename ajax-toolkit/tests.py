6720e48d8c95d5de4ae183ca583cab81fb53585e
Eleni Lixourioti <inelegr88@gmail.com>
2012-06-12 17:38:21 +0100

    middleware and tests

---
+++ ajax-toolkit/tests.py
@@ -0,0 +1,42 @@
+# coding=UTF-8
+
+from django.test import TestCase
+
+from django.contrib import messages
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.contrib.messages.middleware import MessageMiddleware
+
+from hamcrest import *
+
+from ajax-toolkit.middleware import AjaxMiddleware
+from ajax-toolkit.http import JsonResponse
+
+class JsonResponseTests(TestCase):
+
+    def test_response_rendering(self):
+        response = JsonResponse({'foo': 'bar'})
+        response.render()
+        assert_that(response.content, is_('{"foo": "bar"}'))
+
+        response = JsonResponse({'foo': u'ελληνικά'})
+        response.render()
+        assert_that(response.content, is_('{"foo": "\u03b5\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac"}'))
+
+
+class AjaxMiddlewareTests(TestCase):
+
+    def test_middleware_appends_messages(self):
+        request = HttpRequest()
+        request.META['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest'
+        SessionMiddleware().process_request(request)
+        MessageMiddleware().process_request(request)
+
+        message = "Hello. Yes. This is dog"
+        messages.info(request, message)
+
+        response = JsonResponse()
+        middleware = AjaxMiddleware()
+        processed_response = middleware.process_template_response(request, response)
+
+        assert_that(processed_response.dict_content["django_messages"][0], has_entry("message", equal_to(message)))
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='django-ajax-toolkit',
+      version='',
+      url='https://github.com/Geekfish/django-ajax-toolkit',
+      author="Eleni Lixourioti",
+      author_email="contact@eleni.co",
+      description="",
+      long_description=open('README.rst').read(),
+      keywords="Ajax utilities for Django",
+      license='BSD',
+      platforms=['linux'],
+      packages='ajax-toolkit'),
+      include_package_data=True,
+      install_requires=[
+          'simplejson==2.5.2',
+          ],
+      # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Development Status :: 3 - Alpha',
+                   'Programming Language :: Python']
+      )
