a2303912cc864ac11f651bd05683604999c1f8c0
Eleni Lixourioti <contact@eleni.co>
2013-01-30 11:28:36 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-ajax-toolkit',
-      version='0.1',
+      version='0.2',
       url='https://github.com/Geekfish/django-ajax-toolkit',
       author="Eleni Lixourioti",
       author_email="contact@eleni.co",
